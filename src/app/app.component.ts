dd1bd199ebbd7aa7b1471a7b4e94dd5ad3de0e46
Anand Kumar <AKumar8@FreedomMobile.ca>
2019-03-26 11:44:08 -0400

    After updating the npm packages.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-<<<<<<< HEAD
-  title = 'Anand Kumar';
-=======
   title = 'Anand kumar S';
->>>>>>> 6821a81... Second time Title change
 }
